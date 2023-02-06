17bc6ed55e5e27554197273b3543754833bd0a73
Rafael Ramirez <mrsblast@gmail.com>
2023-02-06 12:04:47 -0400

    feat: Add User mongodb schema

---
+++ src/autenticacion/models/user.schema.ts
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+import { Schema, Model, Document } from 'mongoose';
+import { UserModel } from './user.model';
+import UserInterface from './user.interface';
+import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+
+class UserSchema {
+    static instance: UserSchema;
+    static userModel: Model<UserModel>;
+    userSchema: Schema<UserModel>;
+
+    private constructor() {
+        dotenv.config();
+        this.userSchema = new mongoose.Schema({
+            email: {
+                type: String,
+                required: true,
+                index: {
+                    unique: true
+                }
+            },
+            password: {
+                type: String,
+                required: true
+            }
+        });
+
+        this.schemaMethods();
+        this.preSave();
+        UserSchema.userModel = mongoose.model<UserModel>('User', this.userSchema);
+
+    }
+
+    public static getInstance() {
+        if(!this.instance) {
+            UserSchema.instance = new UserSchema();
+        }
+        return UserSchema.instance;
+    }
+
+    async preSave() {
+        this.userSchema.pre('save', async function (next) {
+            const user = this;
+            const salt: number = parseInt(process.env.SALT_WORK_FACTOR);
+            if(this.isModified('password')) {
+                const saltFactor = await bcrypt.genSalt(salt);
+                const hashPassword = await bcrypt.hash(user.password, saltFactor);
+                user.password = hashPassword;
+            } 
+            next();
+        });
+    }
+
+    schemaMethods() {
+        this.userSchema.methods.comparePassword = async function (this: UserModel, password: string): Promise<boolean> {
+            if(this) {
+                const result: boolean = await bcrypt.compare(password, this.password);
+                return result;
+            }
+        }
+    }
+
+
+    async create(attr: UserInterface) {
+        return UserSchema.userModel.create(attr);
+    }
+}
+
+export default UserSchema;
